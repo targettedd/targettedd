27859f778e1f237f53896204de923822cfea024e
targettedd <93074304+targettedd@users.noreply.github.com>
2022-09-01 13:19:43 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "agi4.py",
+    version = "0.1",
+    py_modules = ['agi4'],
+    url = "https://github.com/agitama1/agitama1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
